e395ef62708f299bebb2b7932449d464b3a05509
Shibatsui <68383417+Shibatsui@users.noreply.github.com>
2020-07-23 22:41:51 +0300

    release version

---
+++ src/com/shibatsu/RandomizingEye/Main.java
@@ -0,0 +1,97 @@
+package com.shibatsu.RandomizingEye;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.bukkit.FluidCollisionMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin implements Listener {
+	public Set<Material> allMaterials = new HashSet<Material>();
+	public Set<Material> materialsBlackList = new HashSet<Material>();
+	public Set<String> activePlayers = new HashSet<String>();
+	public Map<String,Block> lastBlocks = new HashMap<String,Block>();
+	Random rand = new Random();
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onEnable() {
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.getCommand("RandomizingEye").setTabCompleter(new Tab());
+		materialsBlackList.add(Material.END_PORTAL);
+		materialsBlackList.add(Material.END_PORTAL_FRAME);
+		materialsBlackList.add(Material.NETHER_PORTAL);
+		materialsBlackList.add(Material.END_CRYSTAL);
+		materialsBlackList.add(Material.AIR);
+		materialsBlackList.add(Material.BEDROCK);
+		materialsBlackList.add(Material.END_GATEWAY);
+		materialsBlackList.add(Material.OBSIDIAN);
+		
+		for(Material material : Material.values()) {
+			if(!materialsBlackList.contains(material) && material.isBlock() && !material.isLegacy()) {
+				allMaterials.add(material);
+			}
+		}
+	}
+	
+	@Override
+	public void onDisable() {
+
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (label.equalsIgnoreCase("RandomizingEye")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("[RandomizingEye] " + "Sorry! This command is players only!");
+				return false;
+			}
+			if (args[0].equalsIgnoreCase("start")) {
+				activePlayers.add(sender.getName());
+				sender.sendMessage("[RandomizingEye] " + "You have recieved power of Randomizing Eye!");
+				return true;
+			}
+			if (args[0].equalsIgnoreCase("stop")) {
+				activePlayers.remove(sender.getName());
+				if(lastBlocks.containsKey(sender.getName())) {
+					lastBlocks.remove(sender.getName());
+				}
+				sender.sendMessage("[RandomizingEye] " + "You have lost control on Randomizing Eye!");
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent event) {
+		if(!activePlayers.contains(event.getPlayer().getName())) {
+			return;
+		}
+		if(event.getPlayer().rayTraceBlocks(100,FluidCollisionMode.SOURCE_ONLY) == null) {
+			return;
+		}
+		if(!lastBlocks.containsKey(event.getPlayer().getName())) {
+			lastBlocks.put(event.getPlayer().getName(), event.getPlayer().rayTraceBlocks(100,FluidCollisionMode.SOURCE_ONLY).getHitBlock());
+		}
+		Block hitBlock = event.getPlayer().rayTraceBlocks(100,FluidCollisionMode.SOURCE_ONLY).getHitBlock();
+		if(!lastBlocks.get(event.getPlayer().getName()).equals(hitBlock)) {
+			Block tempBlock = lastBlocks.get(event.getPlayer().getName());
+			lastBlocks.put(event.getPlayer().getName(), hitBlock);
+			if(!tempBlock.isEmpty() && !materialsBlackList.contains(tempBlock.getType())) {
+				tempBlock.setType((Material)allMaterials.toArray()[rand.nextInt(allMaterials.size())]);
+			}
+		}
+	}
+}
